e3d789f436632fbb66d7553c2321bcd27f713b47
Савенчук Любовь <sav_lu@gmail.com>
2022-08-25 18:05:18 +0500

    ex3/

---
+++ ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string DayOfWeek = Console.ReadLine();
+int Day = Convert.ToInt32(DayOfWeek);
+if (Day < 6)
+{
+   Console.WriteLine("Этот день рабочий");
+}
+else 
+{
+   Console.WriteLine("Этот день выходной");
+}
