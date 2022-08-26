64f31fbb610073e6fc098572ad1842e0f76896fd
Савенчук Любовь <sav_lu@gmail.com>
2022-08-26 15:12:41 +0500

    ex4/

---
+++ ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход случайно сгенерированное число (от 1 до 1 000 000) и проверяет, кратно ли оно одновременно 7 и 23.
+
+int RandomNumber = new Random().Next(1, 1000000);
+Console.WriteLine(RandomNumber);
+int Result = RandomNumber / (3 * 7);
+if (Result == 0)
+{
+   Console.WriteLine("Кратно");
+
+}
+else
+{
+   Console.WriteLine("Не кратно");
+}
